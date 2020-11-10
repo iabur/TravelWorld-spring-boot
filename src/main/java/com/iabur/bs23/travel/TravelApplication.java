4948dffacd740db0befd650edacb238c25214c9a
iabur <iaburrahman@gmail.com>
2020-11-10 18:22:14 +0600

    setup security, login and global exception and others basic setup.

--- src/main/java/com/iabur/bs23/travel/TravelApplication.java
+++ src/main/java/com/iabur/bs23/travel/TravelApplication.java
@@ -2,6 +2,8 @@ package com.iabur.bs23.travel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class TravelApplication {
@@ -9,5 +11,8 @@ public class TravelApplication {
     public static void main(String[] args) {
         SpringApplication.run(TravelApplication.class, args);
     }
-
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
