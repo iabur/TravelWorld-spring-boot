72c717c389dac235d7d81528d6cf5c082eeef6f9
iabur <iaburrahman@gmail.com>
2020-11-10 17:01:15 +0600

    setup index page

---
+++ src/main/java/com/iabur/bs23/travel/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.iabur.bs23.travel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    @GetMapping("/test")
+    public String testf(){
+        return "/index";
+    }
+}
